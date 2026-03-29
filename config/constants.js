16eba19e00d8b5eaf74592fbddaa5982553feda4
logsewatv-code <logsewatv@gmail.com>
2026-03-29 20:22:56 +0530

    Create backend config/constants.js file with game constants and configurations

---
+++ config/constants.js
@@ -0,0 +1,11 @@
+// Game constants and configurations
+
+const GAME_CONSTANTS = {
+    MAX_PLAYERS: 4,
+    MAX_TIME: 60, // seconds
+    COLORS: ['red', 'green', 'blue', 'yellow', 'orange'],
+    DEFAULT_COLOR: 'red',
+    WINNING_SCORE: 10,
+};
+
+export default GAME_CONSTANTS;
